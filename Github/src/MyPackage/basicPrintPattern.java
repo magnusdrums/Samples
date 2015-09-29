08ffb6bec7490ad5fb40b61ed7dae5f631dec077
magnusdrums <david_olanoreyes@hotmail.com>
2015-09-29 09:05:32 -0400

    Instructions
    Explanation of the exercises were added, also a correction of the
    basicPrintPattern

--- Github/src/MyPackage/basicPrintPattern.java
+++ Github/src/MyPackage/basicPrintPattern.java
@@ -1,15 +1,56 @@
-package MyPackage;
+/*
+ Basic patterns using nested loops 
+ 
+ 1) Pattern 		|	3) Pattern
+ 					|
+ ****				|	****
+ ****				|   *  *
+ ****				|	*  *
+ ****				|	****
+ 
+ 2) Pattern 		|
+ 					|
+ *					|
+ **					|
+ ***				|
+ ****				|
+ 
+ */
 
+package MyPackage;
 public class basicPrintPattern {
 
 	public static void main(String[] args) {
+		//1) Pattern
 		String s = "*";
+		int r,c;
 		
-		for(int i=1; i<=5; i++){
-			s += i;
-			System.out.println(s);
+		for(r=1; r<=4; r++){			
+			for(c=1; c<=4 ;c++){
+				System.out.print(s);
+			}	
+			System.out.println();
 		}
-
+		
+		System.out.println();
+		//2) Pattern	
+			for(r=1; r<=4; r++){	
+				for(c=1; c<=r ;c++){
+						System.out.print(s);
+					}	
+					System.out.println();
+				}
+		System.out.println();
+	    //3) Pattern	
+		for(r=1; r<=4; r++){							
+			for(c=1; c<=4 ;c++){
+				if(r==1 || r==4 || c==1 || c==4){
+					System.out.print(s);	
+					} else {
+					System.out.print(" ");
+					}					
+				}	
+				System.out.println();
+			}	
 	}
-
 }
